328e45560a0fd5feb19dd24cc5cceeea473ef17e
Max <maksymilianrozanski@gmail.com>
2018-05-20 13:27:19 +0200

    add Constants class

---
+++ app/src/main/java/maksymilianrozanski/github/io/medicinesbox/model/Constants.kt
@@ -0,0 +1,11 @@
+package maksymilianrozanski.github.io.medicinesbox.model
+
+val DATABASE_VERSION: Int = 1
+val DATABASE_NAME: String = "medicines.db"
+val TABLE_NAME = "medicines"
+
+val KEY_ID = "id"
+val KEY_NAME = "medicine_name"
+val KEY_QUANTITY = "medicine_quantity"
+val KEY_DAILY_USAGE = "medicine_daily_usage"
+val KEY_SAVED_TIME = "medicine_saved_time"
