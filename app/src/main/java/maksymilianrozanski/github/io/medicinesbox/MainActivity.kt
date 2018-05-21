e170dd570d2f5855421baacbf8fa5eb4a0648894
Max <maksymilianrozanski@gmail.com>
2018-05-21 23:58:51 +0200

    add displaying data from database

--- app/src/main/java/maksymilianrozanski/github/io/medicinesbox/MainActivity.kt
+++ app/src/main/java/maksymilianrozanski/github/io/medicinesbox/MainActivity.kt
@@ -1,24 +1,62 @@
 package maksymilianrozanski.github.io.medicinesbox
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.Menu
 import android.view.MenuItem
-
 import kotlinx.android.synthetic.main.activity_main.*
+import maksymilianrozanski.github.io.medicinesbox.data.MedicinesAdapter
+import maksymilianrozanski.github.io.medicinesbox.data.MedicinesDatabaseHandler
+import maksymilianrozanski.github.io.medicinesbox.model.Medicine
 
 class MainActivity : AppCompatActivity() {
+
+    private var adapter: MedicinesAdapter? = null
+    private var medicineListFromDb: ArrayList<Medicine>? = null
+    private var medicineListForAdapter: ArrayList<Medicine>? = null
+
+    private var layoutManger: RecyclerView.LayoutManager? = null
+    var databaseHandler: MedicinesDatabaseHandler? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        databaseHandler = MedicinesDatabaseHandler(this)
+
+        //insert example data
+        var exampleMedicine = Medicine()
+        exampleMedicine.name = "Paracetamol"
+        exampleMedicine.quantity = 10
+        exampleMedicine.dailyUsage = 1
+        exampleMedicine.savedTime = 1526835161326L
+        databaseHandler!!.createMedicine(exampleMedicine)
+
+        medicineListFromDb = ArrayList()
+        medicineListForAdapter = ArrayList()
+        layoutManger = LinearLayoutManager(this)
+        adapter = MedicinesAdapter(medicineListForAdapter!!, this)
+
+        recyclerViewId.layoutManager = layoutManger
+        recyclerViewId.adapter = adapter
+
+        medicineListFromDb = databaseHandler!!.readMedicines()
+        medicineListFromDb!!.reverse()
+
+        for (medicine in medicineListFromDb!!.iterator()) {
+            val currentMedicine = Medicine()
+            currentMedicine.id = medicine.id
+            currentMedicine.name = medicine.name
+            currentMedicine.quantity = medicine.quantity
+            currentMedicine.dailyUsage = medicine.dailyUsage
+            currentMedicine.savedTime = medicine.savedTime
+
+            medicineListForAdapter!!.add(currentMedicine)
         }
+        adapter!!.notifyDataSetChanged()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
---
+++ app/src/main/java/maksymilianrozanski/github/io/medicinesbox/data/MedicinesAdapter.kt
@@ -0,0 +1,50 @@
+package maksymilianrozanski.github.io.medicinesbox.data
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import maksymilianrozanski.github.io.medicinesbox.R
+import maksymilianrozanski.github.io.medicinesbox.model.Medicine
+
+class MedicinesAdapter(private val list: ArrayList<Medicine>, private val context: Context) :
+        RecyclerView.Adapter<MedicinesAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context)
+                .inflate(R.layout.list_row, parent, false)
+        return ViewHolder(view, context, list)
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bindViews(list[position])
+    }
+
+    inner class ViewHolder(itemView: View, context: Context, list: ArrayList<Medicine>)
+        : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+
+        var medicineName = itemView.findViewById(R.id.medicineName) as TextView
+        var medicineQuantity = itemView.findViewById(R.id.medicineQuantity) as TextView
+        var medicineDailyUsage = itemView.findViewById(R.id.medicineDailyUsage) as TextView
+        var medicineSavedTime = itemView.findViewById(R.id.medicineSaveDate) as TextView
+
+        fun bindViews(medicine: Medicine) {
+            medicineName.text = medicine.name
+            medicineQuantity.text = medicine.quantity.toString()
+            medicineDailyUsage.text = medicine.dailyUsage.toString()
+            medicineSavedTime.text = medicine.showFormattedDate()
+        }
+
+        override fun onClick(view: View?) {
+            Toast.makeText(context, "You clicked ${view.toString()} "
+                    , Toast.LENGTH_SHORT).show()
+        }
+    }
+}
