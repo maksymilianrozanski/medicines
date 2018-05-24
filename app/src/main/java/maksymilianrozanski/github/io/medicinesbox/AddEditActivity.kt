90c90f37265a63eeefbb055124e12ea4c6d55105
Max <maksymilianrozanski@gmail.com>
2018-05-24 23:27:03 +0200

    add AddEditActivity, and its layout file

---
+++ app/src/main/java/maksymilianrozanski/github/io/medicinesbox/AddEditActivity.kt
@@ -0,0 +1,12 @@
+package maksymilianrozanski.github.io.medicinesbox
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class AddEditActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_edit)
+    }
+}
