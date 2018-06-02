8af1eefb1e7485318776c7cdcf23d0145e6c25e8
Max <maksymilianrozanski@gmail.com>
2018-06-02 18:50:18 +0200

    add displaying data in AddEditView, after clicking edit button

--- app/src/main/java/maksymilianrozanski/github/io/medicinesbox/AddEditActivity.kt
+++ app/src/main/java/maksymilianrozanski/github/io/medicinesbox/AddEditActivity.kt
@@ -1,12 +1,29 @@
 package maksymilianrozanski.github.io.medicinesbox
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_add_edit.*
+import maksymilianrozanski.github.io.medicinesbox.model.KEY_ID
+import maksymilianrozanski.github.io.medicinesbox.model.Medicine
 
 class AddEditActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_edit)
+
+        var launchIntent: Intent = intent
+        var medicine = launchIntent.getParcelableExtra<Medicine>(KEY_ID)
+
+        if (medicine != null
+                && medicine.name != null
+                && medicine.id != null
+                && medicine.quantity != null
+                && medicine.dailyUsage != null) {
+            medicineNameEditText.setText(medicine.name)
+            medicineQuantityEditText.setText(medicine.quantity!!.toString())
+            medicineDailyUsageEditText.setText(medicine.dailyUsage!!.toString())
+        }
     }
 }
