0dfee08d39d0bda5fe1248d90052bb9c2d5d53e5
Max <maksymilianrozanski@gmail.com>
2018-06-20 19:50:40 +0200

    fix displaying incorrect quantity in some specific saved times

--- app/src/main/java/maksymilianrozanski/github/io/medicinesbox/utilities/QuantityCalculator.kt
+++ app/src/main/java/maksymilianrozanski/github/io/medicinesbox/utilities/QuantityCalculator.kt
@@ -1,25 +1,48 @@
 package maksymilianrozanski.github.io.medicinesbox.utilities
 
+import android.support.annotation.VisibleForTesting
 import maksymilianrozanski.github.io.medicinesbox.data.TimeProvider
 import maksymilianrozanski.github.io.medicinesbox.model.Medicine
+import java.util.*
 
 class QuantityCalculator(var timeProvider: TimeProvider) {
+
+    @VisibleForTesting
+    var dayBeginningHour: Int = 0
 
     fun calculateQuantityToday(medicine: Medicine): Double {
         val savedQuantity = medicine.quantity
         val dailyUsage = medicine.dailyUsage
         val savedDate = medicine.savedTime
         if (savedQuantity != null && dailyUsage != null) {
-
-            val timeSinceLastSave = timeProvider.getCurrentTimeInMillis() - savedDate
-            val daysSinceLastSave = (timeSinceLastSave / 86400000).toInt()
-            val medicineUsed = dailyUsage.times(daysSinceLastSave)
-            val medicineToday = savedQuantity.minus(medicineUsed)
+            val timeAfterSubtraction: Long = subtractTimeAfterCompleteDays(savedDate)
+            val timeSinceLastSave: Long = timeProvider.getCurrentTimeInMillis() - timeAfterSubtraction
+            val daysSinceLastSave = (timeSinceLastSave / 86400000L).toInt()
+            val medicineUsed: Double = dailyUsage.times(daysSinceLastSave)
+            val medicineToday: Double = savedQuantity.minus(medicineUsed)
 
             return if (medicineToday <= 0) {
                 0.0
             } else medicineToday
         }
         return 0.0
+    }
+
+    @VisibleForTesting
+    fun subtractTimeAfterCompleteDays(inputTime: Long): Long {
+        var calendar = Calendar.getInstance()
+        calendar.timeInMillis = inputTime
+
+        calendar.set(Calendar.HOUR_OF_DAY, dayBeginningHour)
+        calendar.set(Calendar.MINUTE, 0)
+        calendar.set(Calendar.SECOND, 0)
+        calendar.set(Calendar.MILLISECOND, 0)
+
+        if (calendar.timeInMillis > inputTime) {
+            calendar.timeInMillis = calendar.timeInMillis - 86400000L
+            return calendar.timeInMillis
+        } else {
+            return calendar.timeInMillis
+        }
     }
 }
--- app/src/test/java/maksymilianrozanski/github/io/medicinesbox/utilities/QuantityCalculatorTest.kt
+++ app/src/test/java/maksymilianrozanski/github/io/medicinesbox/utilities/QuantityCalculatorTest.kt
@@ -59,4 +59,70 @@ class QuantityCalculatorTest {
 
         Assert.assertTrue(quantityCalculator.calculateQuantityToday(medicine).equals(expectedValue))
     }
+
+    @Test
+    fun calculateQuantityTodayTest4(){
+        var timeProvider = TimeProviderMock()
+        var medicine = Medicine()
+        medicine.name = "Paracetamol"
+        medicine.dailyUsage = 1.0
+        medicine.quantity = 3.0
+        medicine.savedTime = 1514775600000L //01 Jan 2018, 04:00:00
+
+        var quantityCalculator = QuantityCalculator(timeProvider)
+        var expectedValue = 1.0
+        Assert.assertTrue(quantityCalculator.calculateQuantityToday(medicine).equals(expectedValue))
+    }
+
+    @Test
+    fun calculateQuantityTodayTest5(){
+        var timeProvider = TimeProviderMock()
+        var medicine = Medicine()
+        medicine.name = "Paracetamol"
+        medicine.dailyUsage = 1.0
+        medicine.quantity = 3.0
+        medicine.savedTime  = 1514840400000L //01 Jan 2018, 22:00
+
+        var quantityCalculator = QuantityCalculator(timeProvider)
+        var expectedValue = 1.0
+        Assert.assertTrue(quantityCalculator.calculateQuantityToday(medicine).equals(expectedValue))
+    }
+
+    @Test
+    fun subtractTimeAfterCompleteDaysTest() {
+        var timeProvider = TimeProviderMock()
+        var quantityCalculator = QuantityCalculator(timeProvider)
+        quantityCalculator.dayBeginningHour = 0
+
+        val inputTime: Long = 1529504725000L //20 Jun 2018, 16:25:25
+        val outputTime: Long = quantityCalculator.subtractTimeAfterCompleteDays(inputTime)
+        val expectedOutput: Long = 1529445600000L   //20 Jun 2018, 00:00:00
+        Assert.assertTrue(outputTime == expectedOutput)
+    }
+
+    @Test
+    fun subtractTimeAfterCompleteDaysTest2() {
+        var timeProvider = TimeProviderMock()
+        var quantityCalculator = QuantityCalculator(timeProvider)
+        quantityCalculator.dayBeginningHour = 10
+
+
+        val inputTime: Long = 1529504725000L //20 Jun 2018, 16:25:25
+        val outputTime: Long = quantityCalculator.subtractTimeAfterCompleteDays(inputTime)
+        val expectedOutput: Long = 1529481600000L    //20 Jun 2018, 10:00:00
+        Assert.assertTrue(outputTime == expectedOutput)
+    }
+
+    @Test
+    fun subtractTimeAfterCompleteDaysTest3() {
+        var timeProvider = TimeProviderMock()
+        var quantityCalculator = QuantityCalculator(timeProvider)
+        quantityCalculator.dayBeginningHour = 22
+
+        val inputTime: Long = 1529504725000L //20 Jun 2018, 16:25:25
+        val outputTime: Long = quantityCalculator.subtractTimeAfterCompleteDays(inputTime)
+        val expectedOutput: Long = 1529438400000L   //19 Jun 2018, 22:00:00
+
+        Assert.assertTrue(outputTime == expectedOutput)
+    }
 }
